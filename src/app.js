6b31a413e4619c5bcf7855df22e436f53fd8e524
Fredy Alexander Camelo <88693018+alex2219@users.noreply.github.com>
2025-03-31 03:26:47 +0000

    excuses generator terminado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,17 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+  const getRandomWord = (arr) => {
+    let result = arr[Math.floor(Math.random() * arr.length)];
+    return result;
+  } 
+
+  let excuse = document.getElementById("excuse");
+  excuse.innerHTML = `${getRandomWord(who)} ${getRandomWord(action)} ${getRandomWord(what)} ${getRandomWord(when)}`;
+
 };
